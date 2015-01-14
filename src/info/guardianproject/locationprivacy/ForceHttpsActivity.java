9530efa41d028b4948cb8ef75d316706c9c4596a
Hans-Christoph Steiner <hans@eds.org>
2015-01-14 22:43:26 +0100

    some location URLs cannot be parsed or fetched, so offer "Force HTTPS"
    Some services use HTTP links to share location, but the location cannot be
    parsed from the URL, nor cannot it be fetched from the site itself without
    substantial code.  Glympse is one such service, since the shared URL is a
    link to a person's location feed, which is Glympse's proprietary system.
    
    closes #4401 https://dev.guardianproject.info/issues/4401

---
+++ src/info/guardianproject/locationprivacy/ForceHttpsActivity.java
@@ -0,0 +1,52 @@
+
+package info.guardianproject.locationprivacy;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+
+/**
+ * This Activity is for location links that cannot be parsed at all, or
+ * ultimately do not produce location information in a way that this app can get
+ * it. So the best we can do is just force HTTPS. For example:
+ * http://glympse.com/09PF-HK30
+ *
+ * @author hans
+ */
+public class ForceHttpsActivity extends Activity {
+    public static final String TAG = "ForceHttpsActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        Log.i(TAG, "onCreate");
+        super.onCreate(savedInstanceState);
+
+        Intent intent = getIntent();
+        if (intent == null) {
+            finish();
+            return;
+        }
+
+        Log.i(TAG, "intent action " + intent.getAction());
+        String categories = "";
+        if (intent.getCategories() != null)
+            for (String category : intent.getCategories())
+                categories += " " + category;
+        Log.i(TAG, "intent categories " + categories);
+
+        Uri uri = intent.getData();
+        Log.i(TAG, "uri: " + uri);
+        if (uri != null) {
+            Uri.Builder builder = uri.buildUpon();
+            builder.scheme("https");
+            intent.setData(builder.build());
+        }
+
+        Log.i(TAG, "startActivity uri: " + intent.getData());
+        intent.setComponent(null); // prompt user for app to view new URI
+        startActivity(intent);
+        finish();
+    }
+}
