2ad2dd82d30f468db758ab916aa2696903e9320a
Hans-Christoph Steiner <hans@eds.org>
2015-01-21 09:30:31 +0100

    get the location from a URI by reading the Redirect HTTP header
    A lot of location shortlinks are just a standard HTTP Redirect, so it is
    easy to just ask for the HTTP Header, and grab the Redirect URI from it.
    Then the location is then parsed from that URI.

---
+++ src/info/guardianproject/locationprivacy/GetGeoFromRedirectUri.java
@@ -0,0 +1,109 @@
+
+package info.guardianproject.locationprivacy;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.net.http.AndroidHttpClient;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.widget.Toast;
+
+import java.io.IOException;
+
+import net.osmand.util.GeoPointParserUtil;
+import net.osmand.util.GeoPointParserUtil.GeoParsedPoint;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpHead;
+import org.apache.http.client.methods.HttpUriRequest;
+
+/**
+ * Some location short URLs are a simple HTTP Redirect to a URL with location,
+ * so we can just do an HTTP {@code HEAD} request to get the parseable URI, for
+ * example: http://amap.com/0F0i02
+ *
+ * @author hans
+ */
+public class GetGeoFromRedirectUri extends Activity {
+    public static final String TAG = "GetUriFromRedirectActivity";
+
+    private Intent intent;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        Log.i(TAG, "onCreate");
+        super.onCreate(savedInstanceState);
+
+        intent = getIntent();
+        if (intent == null) {
+            finish();
+            return;
+        }
+
+        Log.i(TAG, "intent action " + intent.getAction());
+        String categories = "";
+        if (intent.getCategories() != null)
+            for (String category : intent.getCategories())
+                categories += " " + category;
+        Log.i(TAG, "intent categories " + categories);
+
+        Uri uri = intent.getData();
+        Log.i(TAG, "uri: " + uri);
+        if (uri != null && uri.isHierarchical()) {
+            new RedirectHeaderAsyncTask().execute(uri.toString());
+        } else {
+            Toast.makeText(this, R.string.ignoring_unparsable_url, Toast.LENGTH_SHORT).show();
+        }
+        finish();
+    }
+
+    class RedirectHeaderAsyncTask extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... params) {
+            HttpClient httpClient = AndroidHttpClient.newInstance(getString(R.string.app_name));
+            HttpUriRequest request;
+            HttpResponse response;
+            try {
+                request = new HttpHead(params[0]);
+                response = httpClient.execute(request);
+                Log.i(TAG, "response: " + response);
+                return response.getLastHeader("Location").getValue();
+            } catch (ClientProtocolException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String uriString) {
+            Log.i(TAG, "onPostExecute header " + uriString);
+            super.onPostExecute(uriString);
+
+            if (TextUtils.isEmpty(uriString)) {
+                Toast.makeText(GetGeoFromRedirectUri.this, R.string.ignoring_unparsable_url,
+                        Toast.LENGTH_SHORT).show();
+            } else {
+                GeoParsedPoint point = GeoPointParserUtil.parse(uriString);
+                if (point == null) {
+                    Toast.makeText(GetGeoFromRedirectUri.this,
+                            R.string.ignoring_unparsable_url,
+                            Toast.LENGTH_SHORT).show();
+                } else {
+                    intent.setData(Uri.parse(point.toString()));
+                    Log.i(TAG, "startActivity uri: " + intent.getData());
+                    intent.setComponent(null); // prompt for app to view new URI
+                    startActivity(intent);
+                }
+            }
+            finish();
+        }
+    }
+}
