c836bd0b97b24d8e387093d81541b2da2c456c5b
Hans-Christoph Steiner <hans@eds.org>
2015-03-25 16:30:48 -0400

    move block text and example buttons to separate About page

--- src/info/guardianproject/locationprivacy/MainActivity.java
+++ src/info/guardianproject/locationprivacy/MainActivity.java
@@ -2,12 +2,11 @@
 package info.guardianproject.locationprivacy;
 
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Button;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
     public static final String TAG = "MainActivity";
@@ -16,27 +15,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        final Button osmOrg = (Button) findViewById(R.id.osmOrg);
-        osmOrg.setOnClickListener(new OnClickListener() {
+    }
 
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(Intent.ACTION_VIEW);
-                intent.setData(Uri.parse(osmOrg.getText().toString()));
-                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                startActivity(intent);
-            }
-        });
-        final Button gooGl = (Button) findViewById(R.id.gooGl);
-        gooGl.setOnClickListener(new OnClickListener() {
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
 
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(Intent.ACTION_VIEW);
-                intent.setData(Uri.parse(gooGl.getText().toString()));
-                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                NavUtils.navigateUpFromSameTask(this);
+                return true;
+            case R.id.about:
+                Intent intent = new Intent(this, AboutActivity.class);
                 startActivity(intent);
-            }
-        });
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
